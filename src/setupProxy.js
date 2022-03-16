bcb1f34872f814fd59e20bb11ed40ee3b9958f52
swontech <hschoi@s-onetech.com>
2022-03-16 22:30:54 +0900

    proxy middleware 로그레벨 설정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,24 +1,12 @@
-// const proxy = require('http-proxy-middleware');
-
-// module.exports = function(app) {
-//     app.use(
-//         '/api',
-//         proxy.createProxyMiddleware({
-//             target: 'http://localhost:8080',
-//             changeOrigin: true,
-//             pathRewrite: { '^/api': '' }
-//         })
-//     )
-// }
-
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app){
   app.use(
       createProxyMiddleware('/api', {
-          target: 'http://127.0.0.1:8080',
+          target: process.env.REACT_APP_API_URL,
           changeOrigin: true,
-          pathRewrite: { '^/api': '' }
+          pathRewrite: { '^/api': '' },
+          logLevel: "debug",
       })
   )
 };
