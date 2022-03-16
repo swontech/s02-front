013fc9178c9002c8ec9dc524d702d5cb98cd06fb
swontech <hschoi@s-onetech.com>
2022-03-16 22:28:41 +0900

    static데이터 파일 추가

---
+++ src/lib/staticData/data.js
@@ -0,0 +1,24 @@
+//* 이메일 도메인 리스트
+export const emailDomainList = [
+    {
+        code: "@daum.net",
+        desc: "daum.net"
+    },
+    {
+        code: "@naver.com",
+        desc: "naver.com"
+    },
+];
+
+//* 전화번호 리스트
+export const hpNoList = [
+    {
+        code: "010",
+        desc: "010",
+    },
+    {
+        code: "090",
+        desc: "090"
+    }
+];
+
