527c533316cc48ae67e435048ad5af5cde58af0e
황어진 <lawliet9712@gmail.com>
2022-03-24 15:47:43 +0900

    황어진의 첫번째 커밋

---
+++ src/styles/palette.js
@@ -0,0 +1,14 @@
+export default palette = {
+    blue: "#5068F2",
+    blue2: "#3878e2",
+    bointRed: "#ff4169",
+    mint: "#30ceda",
+    mint2: "32bea6",
+    darkGreen: "#357400",
+    lightGreen: "#65a72c",
+    yellow: "#edcb70",
+    lightGray: "#e8e8e8",
+    darkGray: "#3f3f40",
+    white: "#ffffff",
+    black: "#000000"
+};
