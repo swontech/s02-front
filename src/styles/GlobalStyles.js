ea42ec3bc4fc203fb75737b845f163a5c41ce0ed
minshxx <minseonghyeon.naver.com>
2022-04-04 22:31:45 +0900

    Feat: Global Css 스타일 추가

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -4,10 +4,36 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
     ${reset}
     * {
-    box-sizing: border-box;
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+    html {
+        scroll-behavior: smooth;
+    }
+    img {
+        vertical-align: middle;
     }
     body {
-        font-family: Noto Sans, Noto Sans KR;
+        font-family: "Apple SD Gothic Neo", "NotoSans", "NotoSansKR", "NanumMyeongjo";
+        font-size:14px;
+        color:#000;
+        -webkit-user-select:none;
+        -moz-user-select:none;
+        -ms-user-select:none;
+        user-select:none
+        
+    }
+    li {
+        list-style: none;
+    }
+    a {
+        text-decoration: none;
+    }
+    .clearfix:after {
+        content: "";
+        display: block;
+        clear: both;
     }
 `;
 export default GlobalStyle;
