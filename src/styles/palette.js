a1a0099ccfffa68fef79ba194bb1dcbfe89e23ff
minshxx <minseonghyeon.naver.com>
2022-04-05 23:10:09 +0900

    Chore: 색상 추가

--- src/styles/palette.js
+++ src/styles/palette.js
@@ -3,12 +3,16 @@ const palette = {
     blue2: "#3878e2",
     pointRed: "#ff4169",
     mint: "#30ceda",
-    mint2: "32bea6",
+    mint2: "#32bea6",
+    mint3: "#24ac91",
     darkGreen: "#357400",
     lightGreen: "#65a72c",
     yellow: "#edcb70",
     lightGray: "#e8e8e8",
+    lightGray2: "#a2a2a2",
+    lightGray3: "#b1b1b1",
     darkGray: "#3f3f40",
+    darkGray2: "#747474",
     white: "#ffffff",
     black: "#000000"
 };
